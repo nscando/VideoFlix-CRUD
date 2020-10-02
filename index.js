e1d79415558590aac4fffb37e8ed19b417e2d6b1
nscando <nicolas.scandizzo@gmail.com>
2020-10-02 16:31:46 -0300

    Middleware - Boom integration

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 
 
 //body parser middleware
 app.use(express.json());
 
+//routes
 moviesApi(app);
 
-//errors handlers
+//Catch 404
+app.use(notFoundHandler);
+
+//errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
+
 
 app.listen(config.port, function () {
      console.log(`Listening http://localhost:${config.port}`)
