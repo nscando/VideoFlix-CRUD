2865b49cfe9936d4f2a361e1cb2f98582af5b10f
nscando <nicolas.scandizzo@gmail.com>
2020-10-02 10:53:13 -0300

    Middleware - Error Handler

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+
+
+
 //body parser middleware
 app.use(express.json());
 
 moviesApi(app);
 
+//errors handlers
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
      console.log(`Listening http://localhost:${config.port}`)
